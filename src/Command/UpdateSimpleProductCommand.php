33c90e9beef13c31fa52b64cbffdd07a27ea29d3
Nei Santos <nrauni@gmail.com>
2020-05-01 11:14:07 -0300

    Update simple product command

---
+++ src/Command/UpdateSimpleProductCommand.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+namespace SnowIO\OrderHiveDataModel\Command;
+
+use SnowIO\OrderHiveDataModel\Product\Simple\SimpleProduct;
+
+final class UpdateSimpleProductCommand
+{
+    public static function of(SimpleProduct $simpleProduct): self
+    {
+        $updateProduct = new self;
+        $updateProduct->simpleProduct = $simpleProduct;
+        return $updateProduct;
+    }
+
+    public static function fromJson(array $json): self
+    {
+        $updateProduct = new self;
+        $updateProduct->simpleProduct = SimpleProduct::fromJson($json);
+        return $updateProduct;
+    }
+
+    public function getSimpleProduct(): SimpleProduct
+    {
+        return $this->simpleProduct;
+    }
+
+    public function toJson(): array
+    {
+        return $this->simpleProduct->toJson();
+    }
+
+    /** @var SimpleProduct */
+    private $simpleProduct;
+
+    private function __construct()
+    {
+
+    }
+}
